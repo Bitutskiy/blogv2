ce6bb8eb624926fd6b22d4d8fd253553108c7755
Bitutsky <darmollive@gmail.com>
2019-08-21 15:16:15 +0300

    deliver message method

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,10 +1,10 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-  def daily_mail
-    UserMailer.with(user: User.first).daily_mail
+  test "the truth" do
+    UserMailer.with(
+      user: User.first,
+      posts: Post.where('created_at <= ? AND created_at >= ?', Time.now, Time.now - 1.day),
+      hiden: true).digest_mail
   end
 end
