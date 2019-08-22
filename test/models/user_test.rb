d95dc91394d7225cb7f90cd1480d1efe2a165386
Bitutsky <darmollive@gmail.com>
2019-08-22 05:16:32 +0300

    adding tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,20 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should not save user" do
+    user = User.new
+    assert_not user.save
+  end
+
+  test 'valid user' do
+    user = User.new(password: 'password', email: 'john@example.com')
+    assert user.valid?
+  end
+
+  test 'invalid without email' do
+    user = User.new(password: 'password')
+    refute user.valid?
+    assert_not_nil user.errors[:email]
+  end
+
 end
